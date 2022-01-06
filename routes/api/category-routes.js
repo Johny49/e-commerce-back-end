74cee27455e1362a322716a1a2e5dd54cc021527
Geoffrey Johnson <GeoffJ_1@hotmail.com>
2022-01-05 22:43:49 -0600

    updated route to update a category by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -50,7 +50,7 @@ router.put('/:id', async (req, res) => {
         id: req.params.id,
       },
   });
-  if (!categoryData[0]) { //TODO troubleshoot this
+  if (!categoryData[0]) {
     res.status(404).json({ message: 'No category found with this id' });
     return;
   }
